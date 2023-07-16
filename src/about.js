3fa3cd94c1dedf15a3c4d930462e18831dce4f22
Jaime Mire <129628753+chance-class@users.noreply.github.com>
2023-07-15 21:39:11 -0500

    Add content to About pg

--- src/about.js
+++ src/about.js
@@ -6,4 +6,25 @@ export const loadAbout = () => {
     const main = document.createElement("div");
     main.classList.add("main");
     container.appendChild(main);
+    const header = document.createElement("h2");
+    header.textContent = "About";
+    main.appendChild(header);
+    const info = document.createElement("div");
+    info.classList.add("info");
+    main.appendChild(info);
+    const p1 = document.createElement("p");
+    p1.textContent = "The Leaky Cauldron was built in the 1500s by Daisy Dodderidge. Its true address is number one Diagon Alley, and it is believed to have been built along with the rest of the wizarding street.";
+    info.appendChild(p1);
+    const p2 = document.createElement("p");
+    p2.textContent = "With the imposition of the International Statute of Wizarding Secrecy in 1692, Minister Ulick Gamp permitted the pub to continue its existence as a safe haven and refuge for members of the magical community.";
+    info.appendChild(p2);
+    const p3 = document.createElement("p");
+    p3.textContent = "Gamp was sympathetic to the need of wizards to let off steam during this difficult time, and further agreed to give the landlord permission to let people into Diagon Alley from his backyard, because the shops behind the Leaky Cauldron were now also in need of protection.";
+    info.appendChild(p3);
+    const p4 = document.createElement("p");
+    p4.textContent = "Many moons later, The Leaky Cauldron remains the ideal spot to catch up with wizarding gossip or relax after a day of shopping. We hope you'll join us!";
+    info.appendChild(p4);
+    const footer = document.createElement("p");
+    footer.innerHTML = "Information courtesy of <a href='https://harrypotter.fandom.com/wiki/Leaky_Cauldron/'>The Wiki</a>.";
+    info.appendChild(footer);
   }
