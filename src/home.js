bda72b560e8af3dcbcba2e9044db0ee9fcc7d68d
Jaime Mire <129628753+chance-class@users.noreply.github.com>
2023-07-15 20:42:46 -0500

    Add basic load structure for each load func

--- src/home.js
+++ src/home.js
@@ -1,3 +1,9 @@
 export const loadHome = () => {
-
+  const container = document.querySelector(".container");
+  while (container.lastElementChild) {
+    container.removeChild(container.lastElementChild);
+  }
+  const main = document.createElement("div");
+  main.classList.add("main");
+  container.appendChild(main);
 };
