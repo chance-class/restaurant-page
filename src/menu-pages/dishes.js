02ce40348f7be7aa19d5337244f26d6822f06dc0
Jaime Mire <129628753+chance-class@users.noreply.github.com>
2023-07-18 17:39:11 -0500

    Add content to dishes page

--- src/menu-pages/dishes.js
+++ src/menu-pages/dishes.js
@@ -11,6 +11,85 @@ export const loadDishes = () => {
     const header = document.createElement("h3");
     header.textContent = "Signature Dishes";
     main.appendChild(header);
+
+    const card1 = document.createElement("div");
+    const info1 = document.createElement("div");
+    card1.classList.add("card");
+    info1.classList.add("info");
+    card1.appendChild(info1);
+    const head1 = document.createElement("h4");
+    head1.textContent = "Bangers and Mash";
+    info1.appendChild(head1);
+    const blurb1 = document.createElement("p");
+    blurb1.textContent = "This is a description.";
+    info1.appendChild(blurb1);
+    main.appendChild(card1);
+
+    const card2 = document.createElement("div");
+    const info2 = document.createElement("div");
+    card2.classList.add("card");
+    info2.classList.add("info");
+    card2.appendChild(info2);
+    const head2 = document.createElement("h4");
+    head2.textContent = "Steak and Kidney Pie";
+    info2.appendChild(head2);
+    const blurb2 = document.createElement("p");
+    blurb2.textContent = "This is a description.";
+    info2.appendChild(blurb2);
+    main.appendChild(card2);
+
+    const card3 = document.createElement("div");
+    const info3 = document.createElement("div");
+    card3.classList.add("card");
+    info3.classList.add("info");
+    card3.appendChild(info3);
+    const head3 = document.createElement("h4");
+    head3.textContent = "Pea Soup";
+    info3.appendChild(head3);
+    const blurb3 = document.createElement("p");
+    blurb3.textContent = "This is a description.";
+    info3.appendChild(blurb3);
+    main.appendChild(card3);
+
+    const card4 = document.createElement("div");
+    const info4 = document.createElement("div");
+    card4.classList.add("card");
+    info4.classList.add("info");
+    card4.appendChild(info4);
+    const head4 = document.createElement("h4");
+    head4.textContent = "Yorkshire Pudding";
+    info4.appendChild(head4);
+    const blurb4 = document.createElement("p");
+    blurb4.textContent = "This is a description.";
+    info4.appendChild(blurb4);
+    main.appendChild(card4);
+
+    const card5 = document.createElement("div");
+    const info5 = document.createElement("div");
+    card5.classList.add("card");
+    info5.classList.add("info");
+    card5.appendChild(info5);
+    const head5 = document.createElement("h4");
+    head5.textContent = "Shepherds Pie";
+    info5.appendChild(head5);
+    const blurb5 = document.createElement("p");
+    blurb5.textContent = "This is a description.";
+    info5.appendChild(blurb5);
+    main.appendChild(card5);
+
+    const card6 = document.createElement("div");
+    const info6 = document.createElement("div");
+    card6.classList.add("card");
+    info6.classList.add("info");
+    card6.appendChild(info6);
+    const head6 = document.createElement("h4");
+    head6.textContent = "Toad in the Hole";
+    info6.appendChild(head6);
+    const blurb6 = document.createElement("p");
+    blurb6.textContent = "This is a description.";
+    info6.appendChild(blurb6);
+    main.appendChild(card6);
+
     const mainmenu = document.createElement("button");
     mainmenu.textContent = "Main Menu";
     mainmenu.addEventListener("click", () => {
