0f95c9731061af1f001cffd60681be0e02f1ab16
Jaime Mire <129628753+chance-class@users.noreply.github.com>
2023-07-18 17:24:57 -0500

    Add content for contact page

--- src/contact.js
+++ src/contact.js
@@ -9,4 +9,52 @@ export const loadContact = () => {
   const header = document.createElement("h2");
   header.textContent = "Contact";
   main.appendChild(header);
+  const intro = document.createElement("p");
+  intro.textContent = "You are welcome to leave us a message here, but the Muggle internet does not work very often, so please forgive us if we do not respond in a timely manner. -Tom";
+  main.appendChild(intro);
+
+  const form = document.createElement("form");
+  form.setAttribute("method", "post");
+  form.setAttribute("action", "submit.php");
+  const br = document.createElement("br");
+
+  const name = document.createElement("input");
+  name.setAttribute("type", "text");
+  name.setAttribute("name", "name");
+  name.setAttribute("placeholder", "Name");
+
+  const email = document.createElement("input");
+  email.setAttribute("type", "email");
+  email.setAttribute("name", "email");
+  email.setAttribute("placeholder", "Email");
+
+  const message = document.createElement("textarea");
+  message.setAttribute("name", "message");
+  message.setAttribute("placeholder", "Your message here");
+
+  const submit = document.createElement("button");
+  submit.setAttribute("type", "submit");
+  submit.setAttribute("value", "Submit");
+  submit.textContent = "Submit";
+
+  form.appendChild(name);
+  form.appendChild(br.cloneNode());
+  form.appendChild(email);
+  form.appendChild(br.cloneNode());
+  form.appendChild(message);
+  form.appendChild(br.cloneNode());
+  form.appendChild(submit);
+
+  main.appendChild(form);
+
+  const info = document.createElement("div");
+  const info1 = document.createElement("p");
+  info1.textContent = "Phone: +44 7341 123456";
+  info.appendChild(info1);
+  info.appendChild(br.cloneNode());
+  const info2 = document.createElement("p");
+  info2.textContent = "Address: 753 Charing Cross Road";
+  info.appendChild(info2);
+  main.appendChild(info);
+
 };
