4e8f07af012ecfdc7878da8544cc922cf2f2ba44
Jaime Mire <129628753+chance-class@users.noreply.github.com>
2023-07-22 14:18:00 -0500

    Clean up code formatting

--- src/about.js
+++ src/about.js
@@ -1,30 +1,30 @@
 export const loadAbout = () => {
-    const container = document.querySelector(".container");
-    while (container.lastElementChild) {
-      container.removeChild(container.lastElementChild);
-    }
-    const main = document.createElement("div");
-    main.classList.add("main");
-    container.appendChild(main);
-    const header = document.createElement("h1");
-    header.textContent = "About";
-    main.appendChild(header);
-    const info = document.createElement("div");
-    info.classList.add("info-about");
-    main.appendChild(info);
-    const p1 = document.createElement("p");
-    p1.textContent = "The Leaky Cauldron was built in the 1500s by Daisy Dodderidge. Its true address is number one Diagon Alley, and it is believed to have been built along with the rest of the wizarding street.";
-    info.appendChild(p1);
-    const p2 = document.createElement("p");
-    p2.textContent = "With the imposition of the International Statute of Wizarding Secrecy in 1692, Minister Ulick Gamp permitted the pub to continue its existence as a safe haven and refuge for members of the magical community.";
-    info.appendChild(p2);
-    const p3 = document.createElement("p");
-    p3.textContent = "Gamp was sympathetic to the need of wizards to let off steam during this difficult time, and further agreed to give the landlord permission to let people into Diagon Alley from his backyard, because the shops behind the Leaky Cauldron were now also in need of protection.";
-    info.appendChild(p3);
-    const p4 = document.createElement("p");
-    p4.textContent = "Many moons later, The Leaky Cauldron remains the ideal spot to catch up with wizarding gossip over a gillywater or relax after a day of shopping with some shepherds pie. We hope you'll join us!";
-    info.appendChild(p4);
-    const footer = document.createElement("p");
-    footer.innerHTML = "Information courtesy of <a href='https://harrypotter.fandom.com/wiki/Leaky_Cauldron/'>The Wiki</a>.";
-    info.appendChild(footer);
+  const container = document.querySelector(".container");
+  while (container.lastElementChild) {
+    container.removeChild(container.lastElementChild);
   }
+  const main = document.createElement("div");
+  main.classList.add("main");
+  container.appendChild(main);
+  const header = document.createElement("h1");
+  header.textContent = "About";
+  main.appendChild(header);
+  const info = document.createElement("div");
+  info.classList.add("info-about");
+  main.appendChild(info);
+  const p1 = document.createElement("p");
+  p1.textContent = "The Leaky Cauldron was built in the 1500s by Daisy Dodderidge. Its true address is number one Diagon Alley, and it is believed to have been built along with the rest of the wizarding street.";
+  info.appendChild(p1);
+  const p2 = document.createElement("p");
+  p2.textContent = "With the imposition of the International Statute of Wizarding Secrecy in 1692, Minister Ulick Gamp permitted the pub to continue its existence as a safe haven and refuge for members of the magical community.";
+  info.appendChild(p2);
+  const p3 = document.createElement("p");
+  p3.textContent = "Gamp was sympathetic to the need of wizards to let off steam during this difficult time, and further agreed to give the landlord permission to let people into Diagon Alley from his backyard, because the shops behind the Leaky Cauldron were now also in need of protection.";
+  info.appendChild(p3);
+  const p4 = document.createElement("p");
+  p4.textContent = "Many moons later, The Leaky Cauldron remains the ideal spot to catch up with wizarding gossip over a gillywater or relax after a day of shopping with some shepherds pie. We hope you'll join us!";
+  info.appendChild(p4);
+  const footer = document.createElement("p");
+  footer.innerHTML = "Information courtesy of <a href='https://harrypotter.fandom.com/wiki/Leaky_Cauldron/'>The Wiki</a>.";
+  info.appendChild(footer);
+}
--- src/menu-pages/desserts.js
+++ src/menu-pages/desserts.js
@@ -5,110 +5,110 @@ import pudding from '../images/pudding.png';
 import doughnuts from '../images/doughnuts.png';
 
 export const loadDesserts = () => {
-    const container = document.querySelector(".container");
-    while (container.lastElementChild) {
-      container.removeChild(container.lastElementChild);
-    }
-    const main = document.createElement("div");
-    main.classList.add("main");
-    container.appendChild(main);
-    const header = document.createElement("h2");
-    header.textContent = "Desserts";
-    main.appendChild(header);
-
-    const card1 = document.createElement("div");
-    const info1 = document.createElement("div");
-    card1.classList.add("card");
-    info1.classList.add("info");
-    const image1 = document.createElement("img");
-    image1.src = spotted;
-    image1.classList.add("menu-img");
-    image1.setAttribute("alt", "Picture of spotted dick");
-    card1.appendChild(image1);
-    card1.appendChild(info1);
-    const head1 = document.createElement("h3");
-    head1.textContent = "Spotted Dick";
-    info1.appendChild(head1);
-    const blurb1 = document.createElement("p");
-    blurb1.textContent = "Cake that contains dried raisins and currants, and is topped with our creamy custard.";
-    info1.appendChild(blurb1);
-    const price1 = document.createElement("p");
-    price1.textContent = "6S 11K";
-    price1.classList.add("price");
-    info1.appendChild(price1);
-    main.appendChild(card1);
-
-    const card2 = document.createElement("div");
-    const info2 = document.createElement("div");
-    card2.classList.add("card");
-    info2.classList.add("info");
-    const image2 = document.createElement("img");
-    image2.src = treacle;
-    image2.classList.add("menu-img");
-    image2.setAttribute("alt", "Picture of treacle tart");
-    card2.appendChild(image2);
-    card2.appendChild(info2);
-    const head2 = document.createElement("h3");
-    head2.textContent = "Treacle Tart";
-    info2.appendChild(head2);
-    const blurb2 = document.createElement("p");
-    blurb2.textContent = "Baked until golden crisp, and served with our homemade vanilla ice cream.";
-    info2.appendChild(blurb2);
-    const price2 = document.createElement("p");
-    price2.textContent = "8S";
-    price2.classList.add("price");
-    info2.appendChild(price2);
-    main.appendChild(card2);
-
-    const card3 = document.createElement("div");
-    const info3 = document.createElement("div");
-    card3.classList.add("card");
-    info3.classList.add("info");
-    const image3 = document.createElement("img");
-    image3.src = pudding;
-    image3.classList.add("menu-img");
-    image3.setAttribute("alt", "Picture of chocolate pudding");
-    card3.appendChild(image3);
-    card3.appendChild(info3);
-    const head3 = document.createElement("h3");
-    head3.textContent = "Chocolate Pudding";
-    info3.appendChild(head3);
-    const blurb3 = document.createElement("p");
-    blurb3.textContent = "Made with real milk chocolate and garnished with mint.";
-    info3.appendChild(blurb3);
-    const price3 = document.createElement("p");
-    price3.textContent = "5S 7K";
-    price3.classList.add("price");
-    info3.appendChild(price3);
-    main.appendChild(card3);
-
-    const card4 = document.createElement("div");
-    const info4 = document.createElement("div");
-    card4.classList.add("card");
-    info4.classList.add("info");
-    const image4 = document.createElement("img");
-    image4.src = doughnuts;
-    image4.classList.add("menu-img");
-    image4.setAttribute("alt", "Picture of jam doughnuts");
-    card4.appendChild(image4);
-    card4.appendChild(info4);
-    const head4 = document.createElement("h3");
-    head4.textContent = "Jam Doughnuts";
-    info4.appendChild(head4);
-    const blurb4 = document.createElement("p");
-    blurb4.textContent = "A dessert for every occasion - available in plates of 4, 6, or 8.";
-    info4.appendChild(blurb4);
-    const price4 = document.createElement("p");
-    price4.textContent = "4S 12K+";
-    price4.classList.add("price");
-    info4.appendChild(price4);
-    main.appendChild(card4);
-
-    const mainmenu = document.createElement("button");
-    mainmenu.textContent = "Main Menu";
-    mainmenu.classList.add("mainmenu");
-    mainmenu.addEventListener("click", () => {
-      loadMenu();
-    })
-    main.appendChild(mainmenu);
+  const container = document.querySelector(".container");
+  while (container.lastElementChild) {
+    container.removeChild(container.lastElementChild);
   }
+  const main = document.createElement("div");
+  main.classList.add("main");
+  container.appendChild(main);
+  const header = document.createElement("h2");
+  header.textContent = "Desserts";
+  main.appendChild(header);
+
+  const card1 = document.createElement("div");
+  const info1 = document.createElement("div");
+  card1.classList.add("card");
+  info1.classList.add("info");
+  const image1 = document.createElement("img");
+  image1.src = spotted;
+  image1.classList.add("menu-img");
+  image1.setAttribute("alt", "Picture of spotted dick");
+  card1.appendChild(image1);
+  card1.appendChild(info1);
+  const head1 = document.createElement("h3");
+  head1.textContent = "Spotted Dick";
+  info1.appendChild(head1);
+  const blurb1 = document.createElement("p");
+  blurb1.textContent = "Cake that contains dried raisins and currants, and is topped with our creamy custard.";
+  info1.appendChild(blurb1);
+  const price1 = document.createElement("p");
+  price1.textContent = "6S 11K";
+  price1.classList.add("price");
+  info1.appendChild(price1);
+  main.appendChild(card1);
+
+  const card2 = document.createElement("div");
+  const info2 = document.createElement("div");
+  card2.classList.add("card");
+  info2.classList.add("info");
+  const image2 = document.createElement("img");
+  image2.src = treacle;
+  image2.classList.add("menu-img");
+  image2.setAttribute("alt", "Picture of treacle tart");
+  card2.appendChild(image2);
+  card2.appendChild(info2);
+  const head2 = document.createElement("h3");
+  head2.textContent = "Treacle Tart";
+  info2.appendChild(head2);
+  const blurb2 = document.createElement("p");
+  blurb2.textContent = "Baked until golden crisp, and served with our homemade vanilla ice cream.";
+  info2.appendChild(blurb2);
+  const price2 = document.createElement("p");
+  price2.textContent = "8S";
+  price2.classList.add("price");
+  info2.appendChild(price2);
+  main.appendChild(card2);
+
+  const card3 = document.createElement("div");
+  const info3 = document.createElement("div");
+  card3.classList.add("card");
+  info3.classList.add("info");
+  const image3 = document.createElement("img");
+  image3.src = pudding;
+  image3.classList.add("menu-img");
+  image3.setAttribute("alt", "Picture of chocolate pudding");
+  card3.appendChild(image3);
+  card3.appendChild(info3);
+  const head3 = document.createElement("h3");
+  head3.textContent = "Chocolate Pudding";
+  info3.appendChild(head3);
+  const blurb3 = document.createElement("p");
+  blurb3.textContent = "Made with real milk chocolate and garnished with mint.";
+  info3.appendChild(blurb3);
+  const price3 = document.createElement("p");
+  price3.textContent = "5S 7K";
+  price3.classList.add("price");
+  info3.appendChild(price3);
+  main.appendChild(card3);
+
+  const card4 = document.createElement("div");
+  const info4 = document.createElement("div");
+  card4.classList.add("card");
+  info4.classList.add("info");
+  const image4 = document.createElement("img");
+  image4.src = doughnuts;
+  image4.classList.add("menu-img");
+  image4.setAttribute("alt", "Picture of jam doughnuts");
+  card4.appendChild(image4);
+  card4.appendChild(info4);
+  const head4 = document.createElement("h3");
+  head4.textContent = "Jam Doughnuts";
+  info4.appendChild(head4);
+  const blurb4 = document.createElement("p");
+  blurb4.textContent = "A dessert for every occasion - available in plates of 4, 6, or 8.";
+  info4.appendChild(blurb4);
+  const price4 = document.createElement("p");
+  price4.textContent = "4S 12K+";
+  price4.classList.add("price");
+  info4.appendChild(price4);
+  main.appendChild(card4);
+
+  const mainmenu = document.createElement("button");
+  mainmenu.textContent = "Main Menu";
+  mainmenu.classList.add("mainmenu");
+  mainmenu.addEventListener("click", () => {
+    loadMenu();
+  })
+  main.appendChild(mainmenu);
+}
